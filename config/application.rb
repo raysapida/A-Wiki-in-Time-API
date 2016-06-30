07976bb1c5aa6ab2aa491a24b152ac80808f69bd
Ben Goldstein <email": null>
2016-06-29 20:09:09 -0700

    wildcard origins

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Api
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://a-wiki-in-time.herokuapp.com'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
