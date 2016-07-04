ea7132ca9a389ae618a801f5f57772c44076125d
Raymond Sapida <ray.sapida@gmail.com>
2016-07-04 13:48:14 -0700

    Remove unused conde in query controller and extract private methods for params and latlong bounds

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
-  belongs_to :query
-  has_many :queries, through: :queries_events
-  has_many :queries_events
+  # belongs_to :query
+  # has_many :queries, through: :queries_events
+  # has_many :queries_events
+  validates_presence_of :qID, :latitude, :longitude, :event_type
 end
