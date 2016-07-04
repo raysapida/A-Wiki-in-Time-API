c7508395018763280767d0428b2f7f374d15fa2a
Raymond Sapida <ray.sapida@gmail.com>
2016-07-04 14:53:19 -0700

    Add scopes for different event types

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,13 @@ class Event < ApplicationRecord
   # has_many :queries, through: :queries_events
   # has_many :queries_events
   validates_presence_of :qID, :latitude, :longitude, :event_type
+  scope :battles_and_sieges, ->(start, finish) {
+    where(event_type: ['battle', 'siege']).where(scraped_date: start..finish) 
+  }
+  scope :archaeological_sites, -> { where(event_type: 'archaeological_sites')  }
+  scope :explorers, -> { where(event_type: 'explorer') }
+  scope :natural_disasters, ->(start, finish) {
+    where(event_type: ['earthquake', 'volcano', 'tornado'])
+    .where(point_in_time: DateTime.new(start)..DateTime.new(finish))
+  }
 end
