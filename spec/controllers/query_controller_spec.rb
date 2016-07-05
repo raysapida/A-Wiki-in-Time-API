c8f18a795debdcdf133f04c23245c17a1055a5b0
Raymond Sapida <ray.sapida@gmail.com>
2016-07-05 14:57:11 -0700

    Add a test for multiple event types

--- spec/controllers/query_controller_spec.rb
+++ spec/controllers/query_controller_spec.rb
@@ -140,6 +140,53 @@ RSpec.describe QueryController, :type => :controller do
         event = parsed_response['events'].first
         expect(event['qID']).to eq larry.qID
       end
+
+      it 'returns all types of events within the raidus and time span' do
+        params = {
+          radius: '400',
+          lat: '37.819302695136',
+          long: '-122.19968309374997',
+          start_year: '1900',
+          end_year: '1920'
+        }
+
+        sf_earthquake = Event.create({
+          title: '1906 San Francisco earthquake',
+          event_url: 'https://en.wikipedia.org/wiki/1906_San_Francisco_earthquake',
+          qID: 'Q211386',
+          event_type: 'earthquake',
+          point_in_time: DateTime.new(1906),
+          latitude: 37.75,
+          longitude: -122.55
+        })
+
+        larry = Event.create({
+          title: 'Murder of Larry McNabney',
+          event_url: 'https://en.wikipedia.org/wiki/Murder_of_Larry_McNabney',
+          qID: 'Q6937950',
+          event_type: 'assasinations',
+          latitude: 38.59623778636446,
+          longitude: -121.49655809374997
+        })
+
+        robert = Event.create({
+          title: 'Robert S. Williamson',
+          event_url: 'https://en.wikipedia.org/wiki/Robert_S._Williamson',
+          qID: 'Q7349523',
+          event_type: 'explorer',
+          latitude: 37.778067677511686,
+          longitude: -122.41803636523434
+        })
+
+        post :create, params: params, xhr: true
+
+        parsed_response = JSON.parse(response.body)
+        events = parsed_response['events']
+        qIDs = events.map{|event| event['qID']}
+        expect(qIDs).to include sf_earthquake.qID
+        expect(qIDs).to include larry.qID
+        expect(qIDs).to include robert.qID
+      end
     end
   end
 
