47e73ed6c6fdd53aaaacc92ef7b46964f9769ca3
Raymond Sapida <ray.sapida@gmail.com>
2016-07-05 12:03:03 -0700

    Add a spec testing that a natural disaster is returned

--- spec/controllers/query_controller_spec.rb
+++ spec/controllers/query_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe QueryController, :type => :controller do
   describe 'POST #create' do
     describe 'with polygon input empty' do
-      let(:valid_params) { {
+      let(:battle_params) { {
         radius: '300',
         lat: '33.280916241006025',
         long: '-84.42868699999997',
@@ -12,18 +12,27 @@ RSpec.describe QueryController, :type => :controller do
         end_year: '1870'
       } }
 
+      let(:disaster_params) { {
+        radius: '300',
+        lat: '37.819302695136',
+        long: '-122.19968309374997',
+        type: 'natural_disasters',
+        start_year: '1900',
+        end_year: '1920'
+      } }
+
       it 'should give a success status' do
-        post :create, params: valid_params, xhr: true
+        post :create, params: battle_params, xhr: true
         expect(response.status).to eq 200
       end
 
       it 'returns an empty array when theres no matching event' do
-        post :create, params: valid_params, xhr: true
+        post :create, params: battle_params, xhr: true
         parsed_response = JSON.parse(response.body)
         expect(parsed_response['events']).to eq []
       end
 
-      it 'returns an event within the given radius' do
+      it 'returns a battle within the given radius' do
         griswoldville = Event.create({
           title: 'Battle of Griswoldville',
           scraped_date: 1864,
@@ -35,10 +44,27 @@ RSpec.describe QueryController, :type => :controller do
         })
 
 
-        post :create, params: valid_params, xhr: true
+        post :create, params: battle_params, xhr: true
         parsed_response = JSON.parse(response.body)
         event = parsed_response['events'].first
         expect(event['qID']).to eq griswoldville.qID
+      end
+
+      it 'returns a disaster  within the given radius' do
+        sf_earthquake = Event.create({
+          title: '1906 San Francisco earthquake',
+          event_url: 'https://en.wikipedia.org/wiki/1906_San_Francisco_earthquake',
+          qID: 'Q211386',
+          event_type: 'earthquake',
+          point_in_time: DateTime.new(1906),
+          latitude: 37.75,
+          longitude: -122.55,
+        })
+
+        post :create, params: disaster_params, xhr: true
+        parsed_response = JSON.parse(response.body)
+        event = parsed_response['events'].first
+        expect(event['qID']).to eq sf_earthquake.qID
       end
     end
   end
